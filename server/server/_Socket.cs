db9d53841fa7df314b8a10d705a36af45baf3f0d
刘 <1349315448@qq.com>
2018-01-09 18:54:20 +0800

    服务端

---
+++ server/server/_Socket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Collections;
+
+namespace server
+{
+  public class _Socket
+  {
+        //储存在线用户
+        public SortedList<string,string> userlist = new SortedList<string, string>();
+        Form1 man = new Form1();
+        public _Socket(string ip,int port)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(ip),port);
+            serverSocket.Bind(ipe);
+            serverSocket.Listen(10);
+            Task task = new Task(() => {
+                Socket userSocket = serverSocket.Accept();
+                Thread thread = new Thread(UserSocket);
+                thread.Start(userSocket);
+            });
+            task.Start();
+        }
+        public void UserSocket(object socket)
+        {
+           
+            Socket usersoket = socket as Socket;
+            Thread thread = new Thread(MesageSocket);
+            thread.Start(usersoket);
+        }
+        public void MesageSocket(object socket)
+        {
+            byte[] byte_ = new byte[1024 * 1024];
+            Socket MessageSocket = socket as Socket;
+            while (true)
+            {
+                MessageSocket.Receive(byte_);
+                man.richTextBox1.AppendText(System.Text.ASCIIEncoding.ASCII.GetString(byte_));
+                
+            }
+        }
+    }
+}
